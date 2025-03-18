1843db9321ae1ebda529481767886eb55cb1e592
Chengcheng <125948900+chengcheng-wang@users.noreply.github.com>
2025-03-18 13:09:32 +0800

    Update and rename isc_meg_plot.py to isc_meg.py

--- isc_meg.py
+++ isc_meg.py
@@ -19,7 +19,7 @@ n_subj = 30
 all_data = []
 for subj_id in range(1,n_subj+1):
     subj = 'sub-0%d' %subj_id if subj_id < 10 else 'sub-%d' %subj_id
-    subj_data = np.load(Data_DIR + '%s_task-baba.npy' % (subj))  # 直接加载每个被试的数据
+    subj_data = np.load(Data_DIR + '%s_task-baba.npy' % (subj))  
     all_data.append(subj_data)
 
 all_data = np.array(all_data)  # (30,1,64,1519001) interval: 10ms
@@ -36,11 +36,11 @@ np.save(DIR + 'isc/meg_median_channel.npy', mean_channel)
 
 n_channels = 64
 isc_matrix = np.zeros((n_channels, n_subj))    #(64,30)
-for c in range(n_channels):     # 遍历所有通道
-    for i in range(n_subj):     # 遍历所有被试
+for c in range(n_channels):     
+    for i in range(n_subj):  
         print('>>>>> preproc subj:%d/30' % (i + 1))
-        subj_channel = data[i, c, :]    # 获取第 i 个被试的 c 通道时间序列
-        isc_matrix[c, i], _ = pearsonr(subj_channel, mean_channel[c,:])  # 计算与 group 平均通道的皮尔逊相关性
+        subj_channel = data[i, c, :]   
+        isc_matrix[c, i], _ = pearsonr(subj_channel, mean_channel[c,:])  
 print(isc_matrix.shape)
 np.save(DIR + 'isc/meg_corr.npy', isc_matrix)
 
@@ -48,7 +48,7 @@ np.save(DIR + 'isc/meg_corr.npy', isc_matrix)
 corr = np.nan_to_num(zscore(isc_matrix, axis=0, nan_policy='omit'))
 # raw = mne.io.read_raw_fif('Analysis/sample/sub-01_task-multitalker_eeg.vhdr', preload=True)
 # info = raw.info
-# montage = mne.channels.read_custom_montage('Analysis/CACS-64.bvef')  # 电极位置
+# montage = mne.channels.read_custom_montage('Analysis/CACS-64.bvef') 
 # info.set_montage(montage)
 
 fif_file_path = "/Volumes/T7_Shield/preproc/sub-20/sub-20_task-baba_desc-preproc_meg.fif"
@@ -56,7 +56,7 @@ epoch = mne.read_epochs(fif_file_path)
 info = epoch.info
 adjacency = combine_adjacency(n_channels)
 print("Adjacency shape:", adjacency.shape)    #(64,64)
-adjacency, ch_names = mne.channels.find_ch_adjacency(info, 'mag')   # 获取通道邻接矩阵
+adjacency, ch_names = mne.channels.find_ch_adjacency(info, 'mag')  
 
 cor_exp = np.expand_dims(corr.T, axis=1)  # (30,1,64)
 print(cor_exp)
@@ -64,7 +64,6 @@ t_val, clusters, p_val, H0 = spatio_temporal_cluster_1samp_test(cor_exp, n_permu
 significant_channels = np.where(p_val < 0.05)[0]  # found 9 clusters
 print("显著的通道索引:", significant_channels)   # [1，6]
 
-# 结果可视化
 # pos_2d = mne.viz.topomap._find_topomap_coords(info, picks=np.arange(len(info.ch_names)))
 pos_2d = _find_topomap_coords(info, picks=np.arange(len(info.ch_names)))
 fig, ax = plt.subplots()
