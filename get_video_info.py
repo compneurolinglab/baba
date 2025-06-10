e21348df89f546edf991bbe4e0c5e528b14b36de
yikeelainewang <yike.elaine.wang@my.cityu.edu.hk>
2025-06-10 14:31:30 +0800

    Update get_video_info.py

--- get_video_info.py
+++ get_video_info.py
@@ -1,12 +1,14 @@
 import ffmpeg
 
 video_path = "22_1726473871.mp4"
-probe = ffmpeg.probe(video_path,v="error", 
-		select_streams="v:0",  # first video stream
-		show_entries="stream=width,height,r_frame_rate")					
+probe = ffmpeg.probe(video_path,
+		     v="error", 
+		     select_streams="v:0", 
+		     show_entries="stream=width,height,r_frame_rate")					
 video_info = probe["streams"][0]
 
-probe = ffmpeg.probe(video_path,v="error", 
-		select_streams="a:0",  # first audio stream
-		show_entries="stream=sample_rate,channels")
+probe = ffmpeg.probe(video_path,
+		     v="error", 
+		     select_streams="a:0",  
+		     show_entries="stream=sample_rate,channels")
 audio_info = probe["streams"][0]
